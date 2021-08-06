b3bd3a53557bd92a68df1862fd061d5c846d93aa
vytcka4 <vytautas.freelance@gmail.com>
2021-08-06 21:59:37 +0300

    commit

--- script.js
+++ script.js
@@ -74,3 +74,8 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// console.log(arr.slice(2));
+
+// console.log(arr.splice(-1));
